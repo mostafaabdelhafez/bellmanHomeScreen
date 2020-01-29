a5bc1a4566962570c650ec0cc3beddbfa4f884f9
Mostafa <mostafa@Macbooks-MacBook-Pro.local>
2020-01-29 02:29:45 +0200

    animate Curvy button

---
+++ BellmanHomeScreen/Utilitites/Extension.swift
@@ -0,0 +1,33 @@
+//
+//  Extension.swift
+//  BellmanHomeScreen
+//
+//  Created by Mostafa on 28/01/2020.
+//  Copyright © 2020 Mostafa. All rights reserved.
+//
+
+import Foundation
+import UIKit
+extension UIView{
+func Anchor(Top:NSLayoutYAxisAnchor?,Left:NSLayoutXAxisAnchor?,Bottom:NSLayoutYAxisAnchor?,Right:NSLayoutXAxisAnchor?,TopPadding:CGFloat,LeftPadding:CGFloat,BottomPadding:CGFloat,RightPadding:CGFloat,Width:CGFloat,Height:CGFloat){
+    translatesAutoresizingMaskIntoConstraints = false
+    
+    if let Top = Top{
+        self.topAnchor.constraint(equalTo: Top, constant: TopPadding).isActive = true
+        
+    }
+    if let Left = Left{
+        self.leftAnchor.constraint(equalTo: Left, constant: LeftPadding).isActive = true}
+    if let Bottom = Bottom{
+        self.bottomAnchor.constraint(equalTo: Bottom, constant: BottomPadding).isActive = true}
+    if let Right = Right{
+        self.rightAnchor.constraint(equalTo: Right, constant: RightPadding).isActive = true}
+    if Width != 0{
+        self.widthAnchor.constraint(equalToConstant: Width).isActive = true
+    }
+    if Height != 0{
+        self.heightAnchor.constraint(equalToConstant: Height).isActive = true
+    }
+
+    }
+}
